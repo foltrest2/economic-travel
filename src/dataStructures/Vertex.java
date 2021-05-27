cee165c303a01128f6b9503627e05cc46e5c831d
Alejandro Coronel Sanchez <alejo.co9@hotmail.com>
2021-05-27 09:23:57 -0500

    Local changes

--- src/dataStructures/Vertex.java
+++ src/dataStructures/Vertex.java
@@ -28,11 +28,11 @@ public class Vertex {
 	}
 
 	public boolean addNeighbour(String name) {
-		return (neighbours.contains(new Vertex(name, indicator))) ? neighbours.add(new Vertex(name, indicator)) : false;
+		return !(neighbours.contains(new Vertex(name, indicator))) ? neighbours.add(new Vertex(name, indicator)) : false;
 	}
 
 	public boolean deleteNeighbour(String name) {
-		return (neighbours.contains(new Vertex(name, indicator))) ? neighbours.remove(new Vertex(name, indicator)) : false;
+		return !(neighbours.contains(new Vertex(name, indicator))) ? neighbours.remove(new Vertex(name, indicator)) : false;
 	}
 
 	public boolean addConnection(Vertex v1, Vertex v2, Edge e) {
