b4f9fdbc30c0c5bff0a9457ffa0ab1ea1352b41e
Kevin Mera <kamcak@hotmail.es>
2021-05-31 00:05:54 -0500

    floyd with edges done, must be debuged to fix rows and columns bug when searching on the edge floyd matrix

--- src/dataStructures/Edge.java
+++ src/dataStructures/Edge.java
@@ -1,22 +1,26 @@
 package dataStructures;
 
+import java.util.ArrayList;
+
 public class Edge {
 
 	private int time;
 	private int cost;
 	private int[] transport;
 	private boolean useThisWay;
+	private ArrayList<Edge> edgesToGo;
 	private Vertex v1;
 	private Vertex v2;
-	
+
 	public Edge(Vertex v1, Vertex v2, int time, int cost, int [] transport) {
 		this.v1 = v1;
 		this.v2 = v2;
 		this.time = time;
 		this.cost = cost;
 		this.transport = transport;
+		edgesToGo = new ArrayList<>();
 	}
-	
+
 	public Vertex getV1() {
 		return v1;
 	}
@@ -64,5 +68,45 @@ public class Edge {
 	public void setUseThisWay(boolean useThis) {
 		this.useThisWay = useThis;
 	}
-	
+
+	public ArrayList<Edge> getEdgesToGo() {
+		return edgesToGo;
+	}
+
+	public void setEdgesToGo(ArrayList<Edge> edgesToGo) {
+		this.edgesToGo = edgesToGo;
+	}
+
+	public void addEdgesToGo(Edge toAdd) {
+		edgesToGo.add(toAdd);
+	}
+
+	public void clearEdgesToGoArray() {
+		if (edgesToGo != null || !edgesToGo.isEmpty()) {
+			edgesToGo.clear();
+		}
+	}
+
+	@Override
+	public String toString() {
+		return ""+time;
+	}
+
+	public String showRoute() {
+		String info = "";
+		info += "------------ Route ------------"+"\n";
+		info += "Go from: \t  \t  To: "+"\n";
+		if (edgesToGo.isEmpty()) {
+			info +=  v1.getName()+ "  <---->" + "\t" + "\t" +v2.getName()+"\n";
+			info += "Both ways duration time: "+time+ " Minutes"+"\n";
+			info += "** Direct Route! **"+"\n";
+		} else {
+			for (int i = 0; i < edgesToGo.size(); i++) {			
+				info += edgesToGo.get(i).getV1().getName() + "\t"+ "<---->" +"\t"+edgesToGo.get(i).getV2().getName()+"\n";
+				info += "With duration time: "+edgesToGo.get(i).getTime()+ " Minutes"+"\n";
+			}
+			info += "** You must travel several ways! **"+"\n";
+		}
+		return info;
+	}
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -11,12 +11,19 @@ public class Main {
 		 TravelGuide tg = new TravelGuide();
 		 try {
 			tg.importData();
+			System.out.println("Normal matrix");
 			tg.printMatrix();
-			System.out.println();
+			System.out.println("Edge matrix");
+			tg.printEdgeMatrix();
+			System.out.println("normal floyd");
 			tg.floydWarshall();
-			System.out.println();
+			System.out.println("Floyd edges");
+			tg.printFloydWarshallEdges();
+			System.out.println("Prim");
 			tg.prim();
 			System.out.println();
+			System.out.println(tg.searchEdgesBetweenPlacesFloyd("Estadio Olimpico Pascual Guerrero","Cristo Rey"));
+//			System.out.println(tg.searchEdgesBetweenPlacesFloyd("El Bochinche","Chipichape"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (EmptyQueueException e) {
