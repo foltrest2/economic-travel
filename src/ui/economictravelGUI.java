38ed075653a46cd8ab2f5fc02b68a06b0ecdb530
Alejandro Coronel Sanchez <alejo.co9@hotmail.com>
2021-05-31 23:38:57 -0500

    Working GUI Controller

--- src/ui/economictravelGUI.java
+++ src/ui/economictravelGUI.java
@@ -1,11 +1,17 @@
 package ui;
 
+import java.io.File;
 import java.io.IOException;
 
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.DialogPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.AnchorPane;
@@ -145,11 +151,57 @@ public class economictravelGUI {
 	private Circle camV;
 	@FXML
 	private Circle iglesiaSanAntonioV;
-
+	ImageView imav;
+	Image i;
+	File f;
+	
+	//constantes con las rutas de las imagenes de cada lugar
+	private static String ACUAPARQUE = "imagenes\\acuaparque.jpg";
+	private static String BIBLIOTECAJORGEGARCES = "imagenes\\bibliotecajorgegarces.jpg";
+	private static String BULEVARDELRIO = "imagenes\\bulevardelrio.jpeg";
+	private static String CAM = "imagenes\\cam.jpg";
+	private static String CATEDRALMETROPOLITANA = "imagenes\\catedralmetropolitana.jpg";
+	private static String CENTROCULTURALCALI = "imagenes\\centroculturalcali.jpg";
+	private static String CHIPICHAPE = "imagenes\\chipichape.jpg";
+	private static String CRISTOREY = "imagenes\\cristorey.jpg";
+	private static String ELBOCHINCHE = "imagenes\\elbochinche.jpg";
+	private static String ELGATODELRIO = "imagenes\\elgatodelrio.jpeg";
+	private static String ELPLANETARIO = "imagenes\\elplanetario.jpg";
+	private static String GOBERNACIONDELVALLE = "imagenes\\gobernaciondelvalle.jpg";
+	private static String HOSPITALUNIVERSITARIO = "imagenes\\hospitaluniversitario.jpg";
+	private static String HOTELDARNNCARLTON = "imagenes\\hoteldarnncarlton.jpg";
+	private static String HOTELINTERNCONTINENTAL = "imagenes\\hotelintercontinental.jpg";
+	private static String HOTELTORREDECALI = "imagenes\\hoteltorredecali.jpeg";
+	private static String IGLESIASANANTONIO = "imagenes\\iglesiasanantonio.jpg";
+	private static String IGLESIASANFRANCISCO = "imagenes\\iglesiasanfrancisco.jpg";
+	private static String JARDINPLAZA = "imagenes\\jardinplaza.jpg";
+	private static String LAERMITA = "imagenes\\laermita.jpeg";
+	private static String LOMADELACRUZ = "imagenes\\lomadelacruz.jpg";
+	private static String MIRADORTRESCRUCES = "imagenes\\miradortrescruces.jpg";
+	private static String MONUMENTOALAVIACION = "imagenes\\monumentoalaaviacion.jpg";
+	private static String MONUMENTOALAMARIA = "imagenes\\monumentoalamariamulata.jpg";
+	private static String MONUMENTOALASOLIDARIDAD = "imagenes\\monumentoalasolidaridad.jpg";
+	private static String MONUMENTOALDEPORTE = "imagenes\\monumentoaldeporte.png";
+	private static String MONUMENTOASEBASTIAN = "imagenes\\monumentosebastiandebelalcazar.jpg";
+	private static String MUSEOARQUEOLOGICO = "imagenes\\museoarqueologicolamerced.jfif";
+	private static String MUSEODELORO = "imagenes\\museodelorocalima.jpg";
+	private static String MUSEODEPARTAMENTALCIENCIA = "imagenes\\museodepartamentalcienciasnaturales.jpg";
+	private static String MUSEOTERTULIA = "imagenes\\museolatertulia.jfif";
+	private static String PARQUEDELASALUD = "imagenes\\parquedelasalud.jpg";
+	private static String PARQUEDELOSPOETAS = "imagenes\\parquedelospoetas.jpg";
+	private static String PASCUALGUERRERO = "imagenes\\pascual guerrero.jpg";
+	private static String PLAZACAICEDO = "imagenes\\plazacaicedo.jpg";
+	private static String PLAZADETOROS = "imagenes\\plazadetoroscañaveralejo.jpg";
+	private static String JAIROVARELA = "imagenes\\plazoletajairovarela.jpeg";
+	private static String TEATROMUNICIPAL = "imagenes\\teatromunicipal.jfif";
+	private static String unicentro = "imagenes\\unicentro.jpeg";
+	private static String UNIDADDEPORTIVA = "imagenes\\unidaddeportivapanamericana.png";
+	private static String ZOOLOGICODECALI = "imagenes\\zoologicodecali.jpg";
+	
 	public economictravelGUI(TravelGuide t) {
 
 		t=tg;
-
+		
 	}
 
 	public void loadMainMenu() throws IOException {
@@ -158,6 +210,7 @@ public class economictravelGUI {
 		Parent mape = fxmlLoader.load();
 		basePane.setCenter(mape);
 		zoom(map);
+		
 
 
 
@@ -192,51 +245,134 @@ public class economictravelGUI {
 
 	@FXML
 	void catedralsanpedro(MouseEvent event) {
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(CATEDRALMETROPOLITANA);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #5");
+		alert.setContentText("Usted seleccionó la catedral metropolitana de San Pedro");
+		alert.showAndWait();
 
 	}
 
 	@FXML
 	void clickOnAcuaparquedelaCaña(MouseEvent event) {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(ACUAPARQUE);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #1");
+		alert.setContentText("Usted seleccionó acuaparque de la caña");
+		alert.showAndWait();
 
 	}
 
 	@FXML
 	void clickOnBibliotecaJoseGarces(MouseEvent event) {
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(BIBLIOTECAJORGEGARCES);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #2");
+		alert.setContentText("Usted seleccionó la biblioteca departamental Jorge Garces Borrero");
+		alert.showAndWait();
 
 	}
 
 	@FXML
 	void clickOnBulevarDelRio(MouseEvent event) {
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(BULEVARDELRIO);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #3");
+		alert.setContentText("Usted seleccionó el bulevar del Rio");
+		alert.showAndWait();
 
 	}
 
 	@FXML
 	void clickOnCam(MouseEvent event) {
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(CAM);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #4");
+		alert.setContentText("Usted seleccionó el CAM");
+		alert.showAndWait();
+
 
 	}
 
 	@FXML
 	void clickOnCentroculturaldecali(MouseEvent event) {
-
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(CENTROCULTURALCALI);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #6");
+		alert.setContentText("Usted seleccionó el centro cultural de Cali");
+		alert.showAndWait();
 	}
 
 	@FXML
 	void clickOnChipichape(MouseEvent event) {
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(CHIPICHAPE);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #40");
+		alert.setContentText("Usted seleccionó el Centro Comercial Chipichape");
+		alert.showAndWait();
 
+	
 	}
 
 	@FXML
 	void clickOnCristoRey(MouseEvent event) {
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(CRISTOREY);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #7");
+		alert.setContentText("Usted seleccionó Cristo Rey");
+		alert.showAndWait();
 
 	}
 
 	@FXML
 	void clickOnElBochinche(MouseEvent event) {
+		
+		Alert alert = new Alert(AlertType.INFORMATION);
+		f = new File(ELBOCHINCHE);
+		i = new Image(f.toURI().toString());
+		imav = new ImageView(i);
+		alert.setGraphic(imav);
+		alert.setHeaderText("Lugar #36");
+		alert.setContentText("Usted seleccionó el restaurante El Bochinche");
+		alert.showAndWait();
 
 	}
 
 	@FXML
 	void clickOnElGatodelRio(MouseEvent event) {
+		
+		
 
 	}
 
